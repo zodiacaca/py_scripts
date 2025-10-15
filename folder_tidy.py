aefd369ddd42797a79b9c2f921805b15b2fb729b
Steven Lee <zodiacaca@gmail.com>
2025-10-16 07:34:47 +0800

    u

--- folder_tidy.py
+++ folder_tidy.py
@@ -1,5 +1,6 @@
 
 import os
+import shutil
 
 names = []
 
@@ -9,6 +10,10 @@ def tidy_folder(folder_paths):
             # print(f"path: {root}, folders: {len(folders)}, count: {len(files)}")
             if len(folders) == 0 and len(files) == 1 and len(root.split("\\")) == 6:
 
+            """
+                old_file = os.path.join(root, files[0])
+                shutil.move(old_file, folder_path)
+            """
 
             """
                 if files[0] in names:
@@ -20,24 +25,22 @@ def tidy_folder(folder_paths):
                     print(new_file)
                 else:
                     names.append(files[0])
-
-            # print(names)
             """
 
             """
             if len(folders) == 0 and len(files) == 0:
                 print(root)
-                os.rmdir(root)
+                # os.rmdir(root)
             """
 
 if __name__ == "__main__":
     folder_paths = set([
-        # r"D:\NAS-01-D\Media",
-        # r"E:\NAS-01-E",
-        # r"F:\NAS-01-F",
-        r"U:\NAS-01-U\[porn]\(JS)",
-        # r"U:\NAS-01-U\GirlsSection",
-        # r"U:\NAS-01-U\impulse"
+        r"D:\NAS-01-D\Media",
+        r"E:\NAS-01-E",
+        r"F:\NAS-01-F",
+        r"U:\NAS-01-U\[porn]",
+        r"U:\NAS-01-U\GirlsSection",
+        r"U:\NAS-01-U\impulse"
     ])
 
     tidy_folder(folder_paths)
